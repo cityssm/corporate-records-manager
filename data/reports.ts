5b921b7276d1926c21614747b1b229747c650172
dangowans <d.gowans@cityssm.on.ca>
2021-06-28 15:19:58 -0400

    report updates

--- data/reports.ts
+++ data/reports.ts
@@ -8,6 +8,7 @@ export const reports: { [reportName: string]: ReportDefinition } = {
       return "select r.recordID, r.recordNumber," +
         " convert(char(10), r.recordDate, 23) as recordDate," +
         " r.recordTitle, r.recordDescription," +
+        " r.party, r.location," +
         " t.tagCSV" +
         " from CR.Records r" +
         " left join CR.RecordTagCSV t on r.recordID = t.recordID" +
@@ -18,10 +19,36 @@ export const reports: { [reportName: string]: ReportDefinition } = {
     paramNames: ["recordTypeKey"]
   },
 
+  recordTypes: {
+    sql: () => {
+      return "select t.recordType," +
+        " year(r.recordDate) as recordYear," +
+        " sum(case when month(r.recordDate) = 1 then 1 else 0 end) as recordCount_jan," +
+        " sum(case when month(r.recordDate) = 2 then 1 else 0 end) as recordCount_feb," +
+        " sum(case when month(r.recordDate) = 3 then 1 else 0 end) as recordCount_mar," +
+        " sum(case when month(r.recordDate) = 4 then 1 else 0 end) as recordCount_apr," +
+        " sum(case when month(r.recordDate) = 5 then 1 else 0 end) as recordCount_may," +
+        " sum(case when month(r.recordDate) = 6 then 1 else 0 end) as recordCount_jun," +
+        " sum(case when month(r.recordDate) = 7 then 1 else 0 end) as recordCount_jul," +
+        " sum(case when month(r.recordDate) = 8 then 1 else 0 end) as recordCount_aug," +
+        " sum(case when month(r.recordDate) = 9 then 1 else 0 end) as recordCount_sep," +
+        " sum(case when month(r.recordDate) = 10 then 1 else 0 end) as recordCount_oct," +
+        " sum(case when month(r.recordDate) = 11 then 1 else 0 end) as recordCount_nov," +
+        " sum(case when month(r.recordDate) = 12 then 1 else 0 end) as recordCount_dec," +
+        " count(recordID) as recordCount" +
+        " from CR.RecordTypes t" +
+        " left join CR.Records r on t.recordTypeKey = r.recordTypeKey" +
+        " where t.isActive = 1" +
+        " and r.recordDelete_datetime is null" +
+        " group by t.recordType, year(r.recordDate)" +
+        " order by t.recordType, recordYear";
+    }
+  },
+
   "table:Records": {
     sql: () => {
       return "select recordID, recordTypeKey, recordNumber, recordDate," +
-        " recordTitle, recordDescription," +
+        " recordTitle, recordDescription, party, location," +
         " recordCreate_userName, recordCreate_datetime," +
         " recordUpdate_userName, recordUpdate_datetime," +
         " recordDelete_userName, recordDelete_datetime" +
