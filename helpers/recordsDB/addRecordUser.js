9e7e51e2cba8ec3232d03537fc5a2e2dffcb8c11
dangowans <d.gowans@cityssm.on.ca>
2021-12-02 15:41:52 -0500

    handle restoring deleted users holding onto unique keys

--- helpers/recordsDB/addRecordUser.js
+++ helpers/recordsDB/addRecordUser.js
@@ -6,22 +6,51 @@ export const addRecordUser = async (recordUserForm, requestSession) => {
     let recordUserID;
     try {
         const pool = await sqlPool.connect(configFns.getProperty("mssqlConfig"));
-        const result = await pool.request()
+        let result = await pool.request()
             .input("recordID", recordUserForm.recordID)
             .input("userName", recordUserForm.userName)
-            .input("recordUserTypeKey", recordUserForm.recordUserTypeKey)
-            .input("recordCreate_userName", requestSession.user.userName)
-            .input("recordUpdate_userName", requestSession.user.userName)
-            .query("insert into CR.RecordUsers" +
-            " (recordID, userName, recordUserTypeKey," +
-            " recordCreate_userName, recordUpdate_userName)" +
-            " output inserted.recordUserID" +
-            " values (@recordID, @userName, @recordUserTypeKey," +
-            " @recordCreate_userName, @recordUpdate_userName)");
-        if (!result.recordset || result.recordset.length === 0) {
-            return undefined;
+            .query("select recordUserID, recordDelete_datetime" +
+            " from CR.RecordUsers" +
+            " where recordID = @recordID" +
+            " and userName = @userName");
+        if (result.recordset && result.recordset.length > 0) {
+            const recordDelete_datetime = result.recordset[0].recordDelete_datetime;
+            if (!recordDelete_datetime) {
+                return undefined;
+            }
+            recordUserID = result.recordset[0].recordUserID;
+            result = await pool.request()
+                .input("recordUserID", recordUserID)
+                .input("recordUserTypeKey", recordUserForm.recordUserTypeKey)
+                .input("recordCreate_userName", requestSession.user.userName)
+                .input("recordUpdate_userName", requestSession.user.userName)
+                .query("update CR.RecordUsers" +
+                " set recordCreate_userName = @recordCreate_userName," +
+                " recordCreate_datetime = getdate()," +
+                " recordUpdate_userName = @recordUpdate_userName," +
+                " recordUpdate_datetime = getdate()," +
+                " recordDelete_datetime = null," +
+                " recordDelete_userName = null" +
+                " where recordUserID = @recordUserID");
         }
-        recordUserID = result.recordset[0].recordUserID;
+        else {
+            result = await pool.request()
+                .input("recordID", recordUserForm.recordID)
+                .input("userName", recordUserForm.userName)
+                .input("recordUserTypeKey", recordUserForm.recordUserTypeKey)
+                .input("recordCreate_userName", requestSession.user.userName)
+                .input("recordUpdate_userName", requestSession.user.userName)
+                .query("insert into CR.RecordUsers" +
+                " (recordID, userName, recordUserTypeKey," +
+                " recordCreate_userName, recordUpdate_userName)" +
+                " output inserted.recordUserID" +
+                " values (@recordID, @userName, @recordUserTypeKey," +
+                " @recordCreate_userName, @recordUpdate_userName)");
+            if (!result.recordset || result.recordset.length === 0) {
+                return undefined;
+            }
+            recordUserID = result.recordset[0].recordUserID;
+        }
     }
     catch (error) {
         debugSQL(error);
